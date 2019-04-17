a19d22957fe168ea0786170f1f44b6acfa387cc9
Abbe-Engers <abbelaptop@gmail.com>
2019-04-17 09:43:44 +0200

    commit code

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "washi gekke kech";
+            string guessedWord = "";
+            int aantalguesses = 3;
+
+            while (guessedWord != secretWord)
+            {
+                if (aantalguesses >= 1)
+                {
+                    Console.Write("Enter guess(" + aantalguesses + "): ");
+                    guessedWord = Console.ReadLine();
+                    aantalguesses = aantalguesses - 1;
+                }
+
+                else
+                {
+                    Console.Write("Je hebt Verloren!");
+                }
+            }
+            Console.WriteLine("You've won!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ rekenmachine/rekenmachine/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wat wilt u uitrekenen?(cijfer 1): ");
+            int getal1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Wat wilt u uitrekenen?(cijfer 2): ");
+            int getal2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("wat wilt u met deze getallen doen?(X, /, -, +)");
+            string rekenengap = Console.ReadLine();
+
+            if (rekenengap == "X")
+            {
+                Console.Write(getal1 * getal2);
+            }
+            else if (rekenengap == "/")
+            {
+                Console.Write(getal1 / getal2);
+            }
+            else if (rekenengap == "-")
+            {
+                Console.Write(getal1 - getal2);
+            }
+            else if (rekenengap == "+")
+            {
+                Console.Write(getal1 + getal2);
+            }
+            else
+            {
+                Console.Write("kan het teken niet vinden, error 404");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

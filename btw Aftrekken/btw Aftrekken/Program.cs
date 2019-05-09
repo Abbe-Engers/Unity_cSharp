e462927a8adf3476da258a2f8cd523cb1b047224
Abbe-Engers <abbelaptop@gmail.com>
2019-05-09 09:43:56 +0200

    lesbrief 7 af
    lesbrief 7 af

---
+++ btw Aftrekken/btw Aftrekken/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btw_Aftrekken
+{
+    class Program
+    {
+        static int BTWPercentage = 25;
+        static void Main(string[] args)
+        {
+            Console.Write("Over hoeveel euro wil je belasting betalen?: ");
+            string aantalEuro = Console.ReadLine();
+            int aantalEuroInt = Convert.ToInt32(aantalEuro);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("#############################");
+            Console.WriteLine(" ");
+            belastingBon(aantalEuroInt, BTWPercentage);
+            Console.WriteLine(" ");
+            Console.WriteLine("#############################");
+            Console.ReadLine();
+        }
+
+        public static void belastingBon(int euro, int BTW)
+        {
+            int factorInt = 100 / BTW;
+            int belastingInt = euro / factorInt;
+            int overigGeld = euro - belastingInt;
+            Console.WriteLine("Aantal BTW: " + belastingInt);
+            Console.WriteLine("");
+            Console.WriteLine("Aantal Euro na BTW betaling: " + overigGeld);
+        }
+    
+    }
+}

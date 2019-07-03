e58108bbe16fa676dc5c4882fde7255e63838420
Abbe-Engers <abbelaptop@gmail.com>
2019-07-03 15:45:02 +0200

    .

--- btw Aftrekken/btw Aftrekken/Mobiel.cs
+++ btw Aftrekken/btw Aftrekken/Mobiel.cs
@@ -6,18 +6,18 @@ namespace btw_Aftrekken
     public class Mobiel
     {
         private int BTWPercentage;
-        private string Type;
-        private string Soort;
-        private string Merk;
-        private string OS;
+        private string type;
+        private string soort;
+        private string merk;
+        private string os;
 
-        public Mobiel(int nBTWPercentage, string nType, string nSoort, string nMerk, string nOS)
+        public Mobiel(int nBTWPercentage, string nType, string nSoort, string nMerk, string nOs)
         {
             BTWPercentage = nBTWPercentage;
-            Type = nType;
-            Soort = nSoort;
-            Merk = nMerk;
-            OS = nOS;
+            type = nType;
+            soort = nSoort;
+            merk = nMerk;
+            os = nOs;
         }
 
         public int GetBTWPercentage()
